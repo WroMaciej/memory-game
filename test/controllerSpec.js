de82883731493b3f7d1c7284bf432d5504f86e99
WroMaciej <maciej.a.wroblewski@capgemini.com>
2018-09-03 10:22:30 +0200

    Small refactor

--- test/controllerSpec.js
+++ test/controllerSpec.js
@@ -4,52 +4,19 @@ describe('tests for controller', function() {
   it('should set model level after start new given level', function() {
     // given
     var newLevel = 10;
-    //model = createSpy().and.callFake(model);
-    //view = createSpy().and.callFake(view);
-    //view = createSpy('viewSpy', view).and.callFake();
-    //modelSpy = jasmine.createSpyObj('model',['setLevel', 'generateLevel']),
-    //view = jasmine.createSpyObj('view', ['newLevel','refreshStatistics']);
-  //  view = spyOn(window, ['newLevel','refreshStatistics']);
-  //spyOn(window, 'view.newLevel');
-  //spyOn(view, 'newLevel').and.callFake(function(){});
-    //spyOn(view, 'refreshStatistics').and.callFake(function(){});
-  //view = jasmine.createSpy().and.callFake(function(){});
-//viewSpy = {};
+    spyOn(view, 'newLevel').and.callFake(function() {});
+    spyOn(view, 'refreshStatistics').and.callFake(function() {});
+    spyOn(model, 'setLevel').and.callFake(function() {});
+    spyOn(model, 'generateLevel').and.callFake(function() {});
 
-
-  //view = jasmine.createSpy('view',  ['newLevel','refreshStatistics']);
-  //model = jasmine.createSpyObj('view',  ['newLevel','refreshStatistics']);
-  spyOn(view, 'newLevel').and.callFake(function(){});
-  spyOn(view, 'refreshStatistics').and.callFake(function(){return true;});
-  spyOn(model, 'setLevel').and.callFake(function(){});
-  spyOn(model, 'generateLevel').and.callFake(function(){});
-
-  // spyOn(view, 'newLevel').and.callThrough();
-  // spyOn(view, 'refreshStatistics').and.callThrough();
-  // spyOn(model, 'setLevel').and.callThrough();
-  // spyOn(model, 'generateLevel').and.callThrough();
-
-
-  //view.newLevel.and.callFake(function(){});
-  //view.refreshStatistics.and.callFake(function(){});
-  //view.and.callFake(function(){});
-    //view.newLevel.and.callFake(function(){});
-    //view.refreshStatistics.and.callFake(function(){});
-      // modelSetLevelSpy = spyOn(model, 'setLevel'),
-      // modelGenerateLevelSpy = spyOn(model, 'generateLevel'),
-       //viewNewLevelSpy = spyOn(view, 'newLevel').and.returnValue(null),
-       //viewStatisticsSpy = spyOn(view, 'refreshStatistics').and.returnValue(null);
     // when
     controller.startLevel(newLevel);
     // then
+    expect(model.generateLevel).toHaveBeenCalled();
+    expect(model.setLevel).toHaveBeenCalledWith(newLevel);
     expect(view.newLevel).toHaveBeenCalled();
     //expect(modelSetLevelSpy).toHaveBeenCalled();
   });
-
-
-
-
-
 
 
 });
